356773a025839c2b9511109f745afb83487a72dd
KalpakLokhande <102809949+KalpakLokhande@users.noreply.github.com>
2024-03-03 23:10:48 +0530

    minor fixes

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ let video;
 
 const ctx = canvas.getContext('2d')
 
-handTrack.load({ flipHorizontal: true, modelSize: "large" }).then(m => {
+handTrack.load({ flipHorizontal : true, modelType: "ssd320fpnlite", imageScaleFactor : 1}).then(m => {
 
     model = m
 
-    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+    navigator.mediaDevices.getUserMedia({ video: true, flipHorizontal : true }).then(stream => {
 
         video = document.createElement('video')
         video.srcObject = stream
-        video.width = canvas.width
-        video.height = canvas.height
+        video.play()
+
 
         handTrack.startVideo(video)
 
@@ -33,44 +33,45 @@ handTrack.load({ flipHorizontal: true, modelSize: "large" }).then(m => {
 
 const animate = () => {
 
-    
     model.detect(video).then(predictions => {
-
+    
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-
+    
         predictions.forEach(pre => {
-
-            if (pre.label === 'point') {
-
-                let center = { x: pre.bbox[0], y: pre.bbox[1] }
+    
+            if (pre.label === 'open') {
+    
+                let center = { x: pre.bbox[0] + pre.bbox[2], y: pre.bbox[1] }
                 ctx.save()
                 ctx.beginPath()
                 ctx.arc(center.x, center.y, 10, 0, Math.PI * 2)
                 ctx.fill()
                 ctx.restore()
-
+    
             }
-            if (pre.label === 'pinch') {
-
-                let center = { x: pre.bbox[0], y: pre.bbox[1] }
+            if (pre.label === 'point') {
+    
+                let center = { x: pre.bbox[0] + pre.bbox[2], y: pre.bbox[1] }
                 ctx.save()
                 ctx.fillStyle = 'red'
                 ctx.beginPath()
                 ctx.arc(center.x, center.y, 10, 0, Math.PI * 2)
                 ctx.fill()
                 ctx.restore()
-
+    
             }
-
+    
         })
-
-
+    
+    
     }).catch(err => {
-
+    
         console.log(err)
-
+    
     })
-
-    requestAnimationFrame(() => animate())
+    
+    requestAnimationFrame(animate)
 
 }
+    
+
